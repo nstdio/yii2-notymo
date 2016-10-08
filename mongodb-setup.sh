2ca236ed3050f607caa522603df917c7a694d002
Edgar Asatryan <nstdio@gmail.com>
2016-10-08 12:45:09 +0400

    Fixing build script.

--- mongodb-setup.sh
+++ mongodb-setup.sh
@@ -7,9 +7,6 @@
 echo "MongoDB Server version:"
 mongod --version
 
-if test -f /sys/kernel/mm/transparent_hugepage/khugepaged/defrag; then
-echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
-fi
 if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
 fi
